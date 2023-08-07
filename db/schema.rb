8efc4fe6487278743d3feb4d4627b7b52c8bb4af
PhilipeeX <dev.philipe@outlook.com>
2023-08-07 17:08:21 -0300

    feat(weapon): model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_163339) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_171942) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.integer "kind"
     t.integer "level"
+  end
+
+  create_table "weapons", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "power_base"
+    t.integer "power_step"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
