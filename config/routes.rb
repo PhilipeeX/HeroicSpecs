c671317098598a98c7a17b190afa2a0fa3ecbb49
PhilipeeX <dev.philipe@outlook.com>
2023-08-07 21:12:55 -0300

    feat(enemy): model and controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create]
   resources :weapons, only: [:index, :create, :destroy, :show]
+
+  resources :enemies, only: [:update, :destroy]
 end
