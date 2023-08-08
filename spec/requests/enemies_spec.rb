ce98552c6c6e52c410657aa4fdaff1bf2763ce60
PhilipeeX <dev.philipe@outlook.com>
2023-08-08 11:38:48 -0300

    test(enemy): rspec tests for the routes were created

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -1,6 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe "Enemies", type: :request do
+  describe "GET /enemies" do
+    context 'when there are enemies' do
+      let!(:enemies) { create_list(:enemy, 5) }
+
+      before { get '/enemies' }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all enemies' do
+        json_response = JSON.parse(response.body)
+        expect(json_response.size).to eq(5)
+      end
+    end
+
+    context 'when there are no enemies' do
+      before { get '/enemies' }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns an empty array' do
+        json_response = JSON.parse(response.body)
+        expect(json_response).to be_empty
+      end
+    end
+  end
+
+  describe 'GET /enemies/:enemy' do
+    it 'returns the correct information of the enemy' do
+      enemy = create(:enemy, name: 'behemooth', level: 15)
+
+      get "/enemies/#{enemy.id}"
+
+      expect(response.body).to include('behemooth')
+      expect(response.body).to include('15')
+    end
+  end
+
+  describe 'POST /enemies' do
+    it 'create enemy correctly' do
+      enemy_params = FactoryBot.attributes_for(:enemy)
+
+      post "/enemies", params: { enemy: enemy_params}
+      expect(response).to have_http_status(:created)
+      expect(Enemy.last).to have_attributes(enemy_params)
+    end
+  end
+
+
   describe "PUT /enemies" do
     context 'when the enemy exist' do
       it 'return status code 200' do
